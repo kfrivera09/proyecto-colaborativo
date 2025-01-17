e56a65e29fe127157363038e18aba13d7419b228
kfrivera09 <kfrivera20@gmail.com>
2025-01-16 23:25:44 -0500

    Add files via upload
    ejercicios node clase 14

---
+++ app.js
@@ -0,0 +1,16 @@
+const curso = {nombre: "FullStack", Nivel: "basico", Dificultad: "Intermedio"}
+
+console.log(curso.nombre)
+
+const array = [1,2,3]
+
+array.push(4)
+
+console.log(array)
+console.log(array.length)
+
+const prom = {nombreC: "matematica", N1: 2, N2: 4, N3: 3}
+
+const nf = (prom.N1+prom.N2+prom.N3)/3
+
+console.log(prom.nombreC,nf)
