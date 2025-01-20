0f4997db137df5169009ee7aa17b124c6e40ab78
Kevin <kfrivera20@gmail.com>
2025-01-20 15:51:35 -0500

    se agrega ejercicio de clase 15 importacion y importacion

---
+++ clase 15 exportacion y importacion/0.0.1.js
@@ -0,0 +1,7 @@
+const math = require('./math');
+
+console.log('Suma:', math.sumar(10, 5));
+console.log('Resta:', math.restar(20, 8));
+console.log('Multiplicación:', math.multiplicar(6, 7));
+console.log('División:', math.dividir(15, 3));
+console.log('División por cero:', math.dividir(15, 0));
---
+++ clase 15 exportacion y importacion/0.0.2.js
@@ -0,0 +1,8 @@
+import { sumar, restar, multiplicar, dividir } from './math.mjs';
+
+console.log('Suma:', sumar(10, 5));
+console.log('Resta:', restar(20, 8));
+console.log('Multiplicación:', multiplicar(6, 7));
+console.log('División:', dividir(15, 3));
+console.log('División por cero:', dividir(15, 0));
+
